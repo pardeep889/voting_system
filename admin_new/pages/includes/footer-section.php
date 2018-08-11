369fa2e8247513a9595daea9d5c2668e606a5123
pardeep889 <proto@pardeep889@hotmai.com>
2018-08-11 18:11:56 +0530

    db new_design_admin

---
+++ admin_new/pages/includes/footer-section.php
@@ -0,0 +1,4 @@
+<div class="pull-right hidden-xs">
+      <a href="http://protolabzit.com"><b>Protolabz eServices</b></a>
+    </div>
+    <strong>Copyright &copy; 2018-2019 <a href="#">Voting System</a>.</strong> All rights reserved.
